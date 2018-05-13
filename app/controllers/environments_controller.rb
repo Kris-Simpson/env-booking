424398d04fcf86de5d619b9b0d228cbb9ceb577e
Kris-Simpson <the.same.kris@gmail.com>
2018-05-13 21:09:01 +0300

    order booked envs by "from" time

--- app/controllers/environments_controller.rb
+++ app/controllers/environments_controller.rb
@@ -1,6 +1,7 @@
 class EnvironmentsController < ApplicationController
   def show
     @environment = Environment.where(environment_params).first
+    @booked_environments = @environment.booked_environments.order(:from)
 
     respond_to do |format|
       format.html
